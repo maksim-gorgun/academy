f6b29f50d0a4784324cc0d0bb145351e3203f8b5
Maksim  Gorgun <maksimg@playtika.com>
2021-07-16 08:19:30 +0300

    Lesson 4 - Strings

---
+++ src/by/academy/lesson4/SecondProgram.java
@@ -0,0 +1,12 @@
+package by.academy.lesson4;
+
+import static java.lang.System.out;
+
+public class SecondProgram {
+    public static void main(String[] args) {
+        String str1 = new String("пошел нахуй");
+        String str2 = "сам пошел нахуй";
+    out.println(str1);
+    out.println(str2);
+    }
+}
